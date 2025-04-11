84155055585f4329d358b6faced9eee18891486f
gabrieldj1997 <gabrieldj1997@gmail.com>
2025-04-11 04:04:52 -0300

    teste3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <title>RMC - Imobiliaria teste2</title>
+    <title>RMC - Imobiliaria teste3</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
