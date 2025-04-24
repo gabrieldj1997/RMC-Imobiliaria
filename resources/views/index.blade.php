dd23e1b5bf103d7354bfa4e7773b2bbec81e587f
Gustavo Joseh <gustavojoseh2@gmail.com>
2025-04-23 21:40:16 -0300

    atualização

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,19 +9,7 @@
 
   @include('layout.header')
 
-  <!-- Seção de busca -->
-  <section class="py-4 bg-light">
-    <div class="container">
-      <form class="row g-2">
-        <div class="col-md-10">
-          <input type="text" class="form-control" placeholder="Buscar por cidade, bairro ou código" />
-        </div>
-        <div class="col-md-2 d-grid">
-          <button class="btn btn-primary">teste</button>
-        </div>
-      </form>
-    </div>
-  </section>
+  @include('layout.busca')
 
   <!-- Lista de imóveis -->
   <main class="py-5">
